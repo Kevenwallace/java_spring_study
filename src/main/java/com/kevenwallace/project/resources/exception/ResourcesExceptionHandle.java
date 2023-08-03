926bf04d841d806b0edf257dfccf64603d497a61
kevenwallace_ <kevenwallace_@hotmail.com>
2023-08-03 00:55:36 -0300

    Adiciona put e delete methods

--- src/main/java/com/kevenwallace/project/resources/exception/ResourcesExceptionHandle.java
+++ src/main/java/com/kevenwallace/project/resources/exception/ResourcesExceptionHandle.java
@@ -7,21 +7,36 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.kevenwallace.project.services.exceptions.EntityNotFoundException;
+import com.kevenwallace.project.services.exceptions.DataBaseException;
+import com.kevenwallace.project.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourcesExceptionHandle {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request ){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request ){
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(status.value());
 		err.setError("Ressource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(err);
+		return ResponseEntity.status(status.value()).body(err);
 	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> DataBase(DataBaseException e, HttpServletRequest request ){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Data Base Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status.value()).body(err);
+	}
+	
 }
--- src/main/java/com/kevenwallace/project/services/CategoriesService.java
+++ src/main/java/com/kevenwallace/project/services/CategoriesService.java
@@ -5,41 +5,67 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kevenwallace.project.dto.CategoryDTO;
 import com.kevenwallace.project.entities.categories;
 import com.kevenwallace.project.repositories.CategoriesRepository;
+import com.kevenwallace.project.services.exceptions.DataBaseException;
+import com.kevenwallace.project.services.exceptions.ResourceNotFoundException;
 
-
-import com.kevenwallace.project.services.exceptions.EntityNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoriesService {
-	
+
 	@Autowired
 	private CategoriesRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List <categories> list =  repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<categories> list = repository.findAll();
 		List<CategoryDTO> ListDto = list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		return ListDto;
 	}
- 
+
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<categories> object = repository.findById(id);
-		categories entity = object.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		categories entity = object.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
-	
+
 	@Transactional()
 	public CategoryDTO insertCategory(CategoryDTO dto) {
 		categories entity = new categories();
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional()
+	public CategoryDTO updateCategory(Long id, CategoryDTO dto) {
+		try {
+			categories entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("id not found" + id);
+		}
+	}
+
+	public void deleteCategory(Long id) {
+		try {
+			if (repository.existsById(id)) {
+				repository.deleteById(id);
+			} else {
+				throw new ResourceNotFoundException("id not found: " + id);
+			}
+		} catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violation");
+		}
 	}
 }
